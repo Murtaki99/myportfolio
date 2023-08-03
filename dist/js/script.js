307ddf417f2fa9d0876abb96a33989670211500f
murtakidev <1902011021@undhari.ac.id>
2023-08-03 17:16:14 +0700

    update my portfolio

--- dist/js/script.js
+++ dist/js/script.js
@@ -2,11 +2,16 @@
 window.onscroll =function(){
     const header = document.querySelector('header');
     const fixnav = header.offsetTop;
+    const gotop  = document.querySelector('#gotop') 
     
     if(window.pageYOffset > fixnav){
         header.classList.add('nav-fixed');
+        gotop.classList.remove('hidden');
+        gotop.classList.add('flex');
     }else{
         header.classList.remove('nav-fixed');
+         gotop.classList.remove('flex');
+        gotop.classList.add('hidden');
     }
 }
 //humberger Menu
@@ -18,3 +23,34 @@ hamburger.addEventListener('click', function(){
     navMenu.classList.toggle ('hidden');
 
 });
+window.addEventListener('click', function (e) {
+    if (!hamburger.contains(e.target) && !navMenu.contains(e.target)) {
+        hamburger.classList.remove('humb-line-active');
+        navMenu.classList.add('hidden');
+    }
+});
+
+// darkmood
+const darktoggle = document.querySelector('#dark-toggle');
+const html = document.querySelector('html');
+
+darktoggle.addEventListener('click', function(){
+    if(darktoggle.checked){
+        html.classList.add('dark');
+        localStorage.theme ='dark';
+    }else{
+        html.classList.remove('dark');
+        localStorage.theme ='light';
+    }
+});
+
+// pindahkan togel sesuai dengan mode
+if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+  darktoggle.checked =true;
+} else {
+  darktoggle.checked =true;
+}
+
+
+
+
